10b55732d4e786e3e0c1036c785dcb7ce05dee4c
Mohd Sohaib <160093489+sohaibazmi101@users.noreply.github.com>
2024-03-14 03:13:06 +0530

    file updated

--- AppFolder/myApp/src/app/app.component.ts
+++ AppFolder/myApp/src/app/app.component.ts
@@ -1,18 +1,18 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet, RouterModule } from '@angular/router';
 import { HomeComponent } from './home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HomeComponent],
+  imports: [RouterOutlet, HomeComponent, RouterModule],
   template: `
   <main>
     <header class="brand-name">
       <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
     </header>
     <section class="content">
-      <app-home></app-home>
+      <router-outlet></router-outlet>
     </section>
   </main>
 `,
