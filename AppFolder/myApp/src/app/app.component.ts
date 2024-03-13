6d9cc74b059f2a69f94a9fb02d9713ad774cba00
Mohd Sohaib <160093489+sohaibazmi101@users.noreply.github.com>
2024-03-13 06:51:38 +0530

    *ngFor in Angular

---
+++ AppFolder/myApp/src/app/app.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet, HomeComponent],
+  template: `
+  <main>
+    <header class="brand-name">
+      <img class="brand-logo" src="/assets/logo.svg" alt="logo" aria-hidden="true">
+    </header>
+    <section class="content">
+      <app-home></app-home>
+    </section>
+  </main>
+`,
+  styleUrl: './app.component.css'
+})
+export class AppComponent {
+  title = 'homes';
+}
